b83118faaf65d8123d8c86966d5563851ff133ea
yhctung <42920413+yhctung@users.noreply.github.com>
2018-11-30 20:57:56 -0600

    with animations

--- app/src/main/java/com/example/ck/flashcard/MainActivity.java
+++ app/src/main/java/com/example/ck/flashcard/MainActivity.java
@@ -1,58 +1,149 @@
 package com.example.ck.flashcard;
 
+import android.animation.Animator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    FlashcardDatabase flashcardDatabase;        // create database
+    List<Flashcard> allFlashcards;              // holds list of Flashcards
+    int currentCardDisplayedIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        flashcardDatabase = new FlashcardDatabase(getApplicationContext()); // guarantee app is initialized
+        allFlashcards = flashcardDatabase.getAllCards();
+
+        // check if list is empty, if not empty can display saved card
+        if (allFlashcards != null && allFlashcards.size() > 0) {
+            ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(0).getQuestion());
+            ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(0).getAnswer());
+        }
+
+        // if question is tapped, show answer
         findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
-                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+            public void onClick(View v) {
+                //findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+                //findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+
+                // for reveal animation
+                View answerSideView = findViewById(R.id.flashcard_answer);
+                View questionSideView = findViewById(R.id.flashcard_question);
+
+                // get the center for the clipping circle
+                int cx = answerSideView.getWidth() / 2;
+                int cy = answerSideView.getHeight() / 2;
+
+                // get the final radius for the clipping circle
+                float finalRadius = (float) Math.hypot(cx, cy);
+
+                // create the animator for this view (the start radius is zero)
+                Animator anim = ViewAnimationUtils.createCircularReveal(answerSideView, cx, cy, 0f, finalRadius);
+
+                // hide the question and show the answer to prepare for playing the animation!
+                questionSideView.setVisibility(View.INVISIBLE);
+                answerSideView.setVisibility(View.VISIBLE);
+
+                anim.setDuration(3000);
+                anim.start();
+
             }
         });
 
-        findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener(){
+        // if answer is tapped, show question
+        findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
                 findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
             }
         });
 
-        // when plus clicked, start AddCardActivity
-        findViewById(R.id.Plus).setOnClickListener(new View.OnClickListener() {
 
+        // when PLUS clicked, start AddCardActivity
+        findViewById(R.id.Plus).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 Intent i = new Intent(MainActivity.this, AddCardActivity.class);
                 i.putExtra("mode", 2); // pass arbitrary data to launched activity
                 startActivityForResult(i, 100);
+                // For lab 4
+                overridePendingTransition(R.anim.right_in, R.anim.left_out);
             }
         });
+
+        // when NEXT is clicked, show next card
+        findViewById(R.id.Next).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // ANIMATION
+                // load animation files
+                final Animation leftOutAnim = AnimationUtils.loadAnimation(v.getContext(), R.anim.left_out);
+                final Animation rightInAnim = AnimationUtils.loadAnimation(v.getContext(), R.anim.right_in);
+
+                // leftout animation first
+                leftOutAnim.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                        // this method is called when the animation first starts
+
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        // this method is called when the animation is finished playing, aka launch right in
+                        findViewById(R.id.flashcard_question).startAnimation(rightInAnim);
+                        // basically, get the right card
+                        // advance our pointer index so we can show the next card
+                        currentCardDisplayedIndex++;
+
+                        // make sure we don't get an IndexOutOfBoundsError if we are viewing the last indexed card in our list
+                        if (currentCardDisplayedIndex > allFlashcards.size() - 1) {
+                            currentCardDisplayedIndex = 0;
+                        }
+
+                        // set the question and answer TextViews with data from the database
+                        ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
+                        ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+                        // we don't need to worry about this method
+                    }
+                });
+
+
+                findViewById(R.id.flashcard_question).startAnimation(leftOutAnim);
+
+            }
+        });
+/*
+        // delete flashcard from database
+        findViewById(R.id.Delete).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flashcardDatabase.deleteCard(((TextView) findViewById(R.id.flashcard_question)).getText().toString());
+                allFlashcards = flashcardDatabase.getAllCards();
+                currentCardDisplayedIndex--;
+            }
+        });
+*/
     }
-
-    /* First shot
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == 100) {
-            String question = data.getExtras().getString("question");
-            String answer = data.getExtras().getString("answer");
-
-            ((TextView) findViewById(R.id.flashcard_answer)).setText(question);
-            ((TextView) findViewById(R.id.flashcard_question)).setText(answer);
-        }
-
-    }
-    */
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -61,13 +152,14 @@ public class MainActivity extends AppCompatActivity {
             // Extract name value from result extras
             String question = data.getExtras().getString("Question");
             String answer = data.getExtras().getString("Answer");
+
             int code = data.getExtras().getInt("code", 0);
+
+            flashcardDatabase.insertCard(new Flashcard(question, answer));
+            allFlashcards = flashcardDatabase.getAllCards();
 
             ((TextView) findViewById(R.id.flashcard_answer)).setText(answer);
             ((TextView) findViewById(R.id.flashcard_question)).setText(question);
         }
     }
-
-
 }
-
